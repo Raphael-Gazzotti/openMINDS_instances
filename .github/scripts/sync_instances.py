344cb07f2ddc6c4ec014f3b3bdb17400478932b6
raphaelgazzotti <raphael.gazzotti@ebrains.eu>
2024-11-04 10:03:49 +0100

    Naive approach that replace properties from a src_file to a target_file, some properties are validated in regard to the version.

--- .github/scripts/sync_instances.py
+++ .github/scripts/sync_instances.py
@@ -1,27 +1,74 @@
 import json
+import re
 import sys
 
+from utils import properties_file, regex_pattern_type, regex_pattern_instance, types_file, version_file
 
-def sync_properties(src_data, tgt_data):
+
+def delete_properties(src_data, tgt_data):
+    """Delete properties from tgt_data not in src_data."""
+    for key in list(tgt_data.keys()):
+        if key not in src_data:
+            del tgt_data[key]
+            print(f"Property removed: '{key}' not found in source data.")
+    return tgt_data
+
+
+def sync_properties(src_data, tgt_data, version):
     """Sync properties from src_data to tgt_data."""
     for key, value in src_data.items():
-        # Skip special keys that should not be modified
-        if key in ["@vocab", "@type", "@id"]:
+        # Check if @type exists for a given version and replace by the appropriate namespace
+        try:
+            if key == "@vocab":
+                tgt_data[key] = version_file[version]["namespaces"]["props"]
+                continue
+            elif key == "@type":
+                if re.match(regex_pattern_type, src_data[key]):
+                    _type = re.sub(regex_pattern_type, "", src_data[key])
+                    tgt_data[key] = [_namespace_version for _namespace_version in types_file[_type]["hasNamespace"] if version in _namespace_version["inVersions"]][0]['namespace'] + _type
+                else:
+                    tgt_data[key] = src_data[key]
+                continue
+            elif key == "@id":
+                if re.match(regex_pattern_instance, src_data[key]):
+                    _id = re.sub(regex_pattern_instance, "", src_data[key])
+                    tgt_data[key] = version_file[version]["namespaces"]["instances"] + _id
+                else:
+                    tgt_data[key] = src_data[key]
+                continue
+        except IndexError:
+            print(f"Type {_type} not found in version {version}.")
+            return
+
+        # Check if a property exists for a given version, if not remove it
+        if key in ["@context", "@type"]:
+            continue
+        elif key in properties_file:
+            try:
+                [_namespace_version for _namespace_version in properties_file[key]["hasNamespace"] if version in _namespace_version["inVersions"]][0]
+            except IndexError:
+                del tgt_data[key]
+                print(f"Property skipped: {key} not found in version {version}.")
+                continue
+        else:
+            del tgt_data[key]
+            print(f"Property skipped: {key} not found.")
             continue
 
         # If the value is a dictionary, recurse
         if isinstance(value, dict):
-            tgt_data[key] = sync_properties(value, tgt_data.get(key, {}))
+            tgt_data[key] = sync_properties(value, tgt_data.get(key, {}), version)
 
+        # Naive approach, just use src_data to build lists
         elif isinstance(value, list):
             if all(isinstance(item, dict) for item in value):
-                tgt_data[key] = tgt_data.get(key, [{}] * len(value))  # Ensure tgt_data has the same length
+                tgt_data[key] = []
                 for idx, item in enumerate(value):
-                    tgt_data[key][idx] = sync_properties(item, tgt_data[key][idx])
+                    tgt_data[key].append({})
+                    tgt_data[key][idx] = sync_properties(item, tgt_data[key][idx], version)
             else:
                 # Non-dictionary values
                 tgt_data[key] = value
-
         else:
             # Update the value in the target for other data types
             tgt_data[key] = value
@@ -29,7 +76,7 @@ def sync_properties(src_data, tgt_data):
     return tgt_data
 
 
-def main(src_file, tgt_file):
+def main(src_file, tgt_file, version):
     # Load source JSON data
     with open(src_file) as f:
         src_data = json.load(f)
@@ -41,18 +88,19 @@ def main(src_file, tgt_file):
     print(f'Synced properties from {src_file} to {tgt_file}')
 
     # Sync properties
-    target_data = sync_properties(src_data, tgt_data)
+    tgt_data = delete_properties(src_data, tgt_data)
+    target_data = sync_properties(src_data, tgt_data, version)
 
     # Write the updated target data back to the file
     with open(tgt_file, 'w') as f:
-        json.dump(target_data, f, indent=4)
+        json.dump(target_data, f, indent=2)
 
     print(f'Successfully synced properties from {src_file} to {tgt_file}')
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python sync_instances.py <src_file> <tgt_file>")
+    if len(sys.argv) != 4:
+        print("Usage: python sync_instances.py <src_file> <tgt_file> <version>")
         sys.exit(1)
 
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
